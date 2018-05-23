c58b78372912a0c6565ed713f3334ce39eccf0af
MXG <1917197716@qq.com>
2018-05-23 14:27:40 +0800

    合并分支

--- src/com/mxg/Demo1.java
+++ src/com/mxg/Demo1.java
@@ -8,6 +8,7 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("张无忌");
         System.out.println("赵敏");
+        System.out.println("张无忌123");
     }
 
     public void xiwu(){
